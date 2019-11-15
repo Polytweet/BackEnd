8c6ca46a427aedac99e276f193f81ae306405086
Aurian <durand.aurian@me.com>
2019-11-15 16:58:58 +0100

    Suppression dossier dist

--- graphql/resolvers/news.js
+++ graphql/resolvers/news.js
@@ -3,7 +3,7 @@ const News = require('../../model/news');
 module.exports = {
     Query: {
         news: async () => {
-            return News.find().sort({date: -1});
+            return News.find().sort({date: -1}).limit(1000);
         },
     },
 };
