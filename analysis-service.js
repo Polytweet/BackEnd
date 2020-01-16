9a8d7706216bc7182b349b6460aeeb9442582f9f
Aurian <durand.aurian@me.com>
2020-01-16 23:53:00 +0100

    Optimisations analysis-service

--- analysis-service.js
+++ analysis-service.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 const matchingDataV0 = require('./matchingDataV0');
 
+var schedule = require('node-schedule');
+
 // const matchingDataV1 = require('./matchingDataV1');
 // const cleanDico = require('./cleanDico');
 
 mongoose.connect(`mongodb://localhost/polytweet-database`, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
   //Version 0 un peu cassée
-  matchingDataV0();  
+  // matchingDataV0();  
+
+  // run the serice every 10 minutes
+  schedule.scheduleJob('*/10 * * * *', function(){
+    matchingDataV0();
+  });
   
   //Version 1 avec utilisation de l'API sans traitement préalable sur les textes
   // matchingDataV1();
