d1f15d65a4de478f03954dd8c7f1fb5af8116e2f
Aurian <durand.aurian@me.com>
2020-02-05 21:37:59 +0100

    Ajouts sous services + maj find preparationData

---
+++ analysis-v1.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+var schedule = require('node-schedule');
+const matchingDataV1 = require('./matchingDataV1');
+
+mongoose.connect(`mongodb://localhost/polytweet-database`, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => { 
+
+  // run the serice every 10 minutes
+  // schedule.scheduleJob('*/10 * * * *', function(){
+  //   matchingDataV1();
+  // });
+
+  //Version 1 avec utilisation de l'API sans traitement préalable sur les textes
+  matchingDataV1();
+
+}).catch(err => {
+  console.log(err);
+});
